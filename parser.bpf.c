64dc1080be1124b1089c69328736aeb44b65ca2e
Laurin Brandner <mail@laurinbrandner.ch>
2024-09-20 09:12:52 +0200

    simplify bpf

--- parser.bpf.c
+++ parser.bpf.c
@@ -12,42 +12,41 @@ char LICENSE[] SEC("license") = "GPL";
 struct {
     __uint(type, BPF_MAP_TYPE_SOCKHASH);
     __uint(max_entries, 1000);
-    __uint(key_size, sizeof(int));
+    __uint(key_size, sizeof(__u32));
     __uint(value_size, sizeof(int));
 } sock_map SEC(".maps");
 
 SEC("sockops")
 int sock_ops(struct bpf_sock_ops *ops) {
-    int op = (int)ops->op;
-    bpf_printk("local: %d, remote: %d, op: %d", ops->local_port, bpf_ntohl(ops->remote_port), op);
+    bpf_printk("sockop: %d (%d -> %d)", ops->op, ops->local_port, bpf_ntohl(ops->remote_port));
 
-    if (bpf_ntohl(ops->remote_port) != port) {
-        return 1;
-    }
+    if (ops->local_port == port) {
+        if (ops->op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || ops->op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
+            if (bpf_sock_hash_update(ops, &sock_map, (void*)&key, BPF_NOEXIST) < 0) {
+                bpf_printk("ERROR: Adding socket failed.");
+            }
 
-    if (op == BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB || op == BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB) {
-        if (bpf_sock_hash_update(ops, &sock_map, (void*)&key, BPF_NOEXIST) < 0) {
-            bpf_printk("ERROR: Adding socket failed.");
+            bpf_printk("Added socket (%d)", key);
         }
-
-        bpf_printk("Added socket (%d)", key);
     }
 
-    return 1;
+    return 0;
 }
 
 SEC("sk_skb/stream_parser")
 int stream_parser(struct __sk_buff *skb) {
-    bpf_printk("Parser %d bytes", skb->len);
+    bpf_printk("Parser %dB (%d -> %d)", skb->len, skb->local_port, bpf_ntohl(skb->remote_port));
     return skb->len;
 }
 
 SEC("sk_skb/stream_verdict")
 int stream_verdict(struct __sk_buff *skb) {
-    bpf_printk("Verdict %d bytes", skb->len);
+    bpf_printk("Verdict %dB (%d -> %d)", skb->len, skb->local_port, bpf_ntohl(skb->remote_port));
 
     #ifdef REDIRECT
-    return bpf_sk_redirect_hash(skb, &sock_map, (void*)&key, 0);
+    int r = bpf_sk_redirect_hash(skb, &sock_map, (void*)&key, BPF_F_INGRESS);
+    bpf_printk("Success: %d", r);
+    return r;
     #else
     return SK_PASS;
     #endif
